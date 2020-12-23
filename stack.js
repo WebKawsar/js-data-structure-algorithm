eeb84b7ff822ae16095ad5c09aa24d11fc5dd518
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-23 22:38:50 +0600

    added

--- stack.js
+++ stack.js
@@ -1,43 +1,100 @@
- const friends = [];
+//  const friends = [];
 
- friends.push(101);
- friends.push(201);
- friends.push(301);
- friends.push(401);
+//  friends.push(101);
+//  friends.push(201);
+//  friends.push(301);
+//  friends.push(401);
  
-//  console.log(friends);
+// //  console.log(friends);
 
-const one = friends.pop()
-// console.log(one);
-// console.log(friends);
+// const one = friends.pop()
+// // console.log(one);
+// // console.log(friends);
 
 
 
-class StackPractice {
-    constructor(){
-        this.stack = [];
-    }
+// class StackPractice {
+//     constructor(){
+//         this.stack = [];
+//     }
 
-    add(item){
-        this.stack.push(item);
-    }
+//     add(item){
+//         this.stack.push(item);
+//     }
 
-    remove(){
-        if(this.stack.length){
-            this.stack.pop();
-        }
-    }
-}
+//     remove(){
+//         if(this.stack.length){
+//             this.stack.pop();
+//         }
+//     }
+// }
 
-const guest = new StackPractice();
-guest.add("Kawsar")
-guest.add("Shamim")
-guest.add("Murad")
-guest.add("Maksud")
+// const guest = new StackPractice();
+// guest.add("Kawsar")
+// guest.add("Shamim")
+// guest.add("Murad")
+// guest.add("Maksud")
 
-console.log(guest.stack);
+// console.log(guest.stack);
 
-guest.remove()
-guest.remove()
+// guest.remove()
+// guest.remove()
 
-console.log(guest.stack);
+// console.log(guest.stack);
+
+// const a = "1"
+// const b = 1
+// const d = 5
+
+
+// const c =  b + d + a
+// console.log(c);
+
+
+// const array = [1, 2, 3]
+// const sum = array.reduce((index, sum) => index + sum, 0)
+// console.log(sum);
+
+// const arr = [1, 2, 3]
+
+// let newArr = arr.map((index) => {
+//     console.log(index);
+// })
+//  console.log(newArr)
+
+
+
+// const array = [1, 2, 3];
+// delete array[0]
+// console.log(array);
+
+// [, 2, 3]
+
+// const def = (a, b, c = "professional") => {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// def("Kawsar", "Ahmed")
+
+// function show(...args) {  
+//     console.log(args); // [10, 20, 30]
+// }  
+    
+// show(10, 20, 30);
+
+// let cagingIt = {
+//     foo: {
+//       bar: 'Nick Cage'
+//     }
+// };
+
+
+// const { foo: { bar } } = cagingIt;
+// console.log(bar);
+
+// const arr = [1 , 2, 3]
+
+// const newArr =  arr.map(index => index)
+// console.log(newArr);
